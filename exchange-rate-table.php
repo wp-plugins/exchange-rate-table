ceb362ce95c3e325659e658d56565602d8fc0621
enclick <enclick@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-07 14:13:05 +0000

    author correction
    git-svn-id: https://plugins.svn.wordpress.org/exchange-rate-table/tags/1.0@329744 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- exchange-rate-table.php
+++ exchange-rate-table.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Exchange Rate Table
 Description: Exchange Rate Table for any currency in the world. Choose the currency to display and the table size and format.
-Author: fx-rate.net
+Author: enclick
 Version: 1.0
 Author URI: http://fx-rate.net
 Plugin URI: http://fx-rate.net/wordpress-exchange-rate-plugin/
